30bcb1a526b804b429bb5f2703649d9c8bac7442
rene_mbp <harryloveslearning@googlemail.com>
2017-06-03 20:35:45 +0200

    travis added

---
+++ scripts/travis-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+gitCommitMessage="travis CI"
+
+echo === start azure deploy
+
+gitRepo="https://${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD}@${AZURE_WA_SITE}.scm.azurewebsites.net:443/${AZURE_WA_SITE}.git"
+
+git clone $gitRepo 
+cd ${AZURE_WA_SITE}
+# remove old files inside the repo
+rm -rf *
+
+git config user.email ${GIT_EMAIL}
+git config user.name ${GIT_NAME}
+
+# copy new files
+cp -r ../dist/. .
+# copy web.config to root
+# cp ../dist/assets/web.config .
+git add .
+git commit -am"${gitCommitMessage}"
+git push > /dev/null 2>&1
+
+cd ..
+
+echo === finished azure deploy

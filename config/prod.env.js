a6308470b410e0f033150fed13fcdd07d0d0dc31
rene_imac <HarryMillerMail@googlemail.com>
2017-06-18 12:51:52 +0200

    debug on prod

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
-  NODE_ENV: '"production"',
+  // NODE_ENV: '"production"',
+  NODE_ENV: '"development"',
   BACKEND_PROJECT_HOST: '"http://v-projectservice.azurewebsites.net"',
   BACKEND_PICTURE_HOST: '"http://v-pictureservice.azurewebsites.net"'
 }

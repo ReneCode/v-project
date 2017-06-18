664abe3c28e06b9965a2ab811bb5a44b8f9569a9
rene_imac <HarryMillerMail@googlemail.com>
2017-06-18 12:04:15 +0200

    home page with project/login logic

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,44 +2,60 @@
   <div class="hello">
     <headline></headline>
     <div class="headline-gap"></div>
-    <router-link to="/projects"><h1>{{ msg }}</h1></router-link>
+    <router-link v-if="isLoggedIn" to="/projects">
+      <h1>Projects</h1>
+    </router-link>
+    <h1 v-if="!isLoggedIn" v-on:click="login">Login</h1>
   </div>
 </template>
 
 <script>
 import Headline from './Headline.vue'
+import auth from "../services/auth-service";
 
 export default {
   name: 'home',
-  data () {
-    return {
-      msg: 'Projects'
-    }
-  },
   components: {
     Headline
+  },
+
+  computed: {
+    isLoggedIn() {
+      return auth.isLoggedIn();
+    }
+  },
+
+  methods: {
+    login() {
+      auth.login();
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped >
 .headline-gap {
-    margin-top: 50px;
+  margin-top: 50px;
 }
-h1 {
-	margin: 150px;
-	padding-left: 40px;
-	padding-top: 80px;
-	padding-bottom: 40px;
-	font-family: "arial";
-	font-size: 56px;
-	font-weight: bold;
-	color: #333;
 
-	border-left: 10px dotted #ccc;
-	border-bottom: 1px dotted #bbb;
+h1 {
+  margin: 150px;
+  padding-left: 40px;
+  padding-top: 80px;
+  padding-bottom: 40px;
+  font-family: "arial";
+  font-size: 56px;
+  font-weight: bold;
+  color: #333;
+
+  border-left: 10px dotted #ccc;
+  border-bottom: 1px dotted #bbb;
+
+  cursor: pointer;
 }
+
 a:hover {
   text-decoration: none;
   color: #333;

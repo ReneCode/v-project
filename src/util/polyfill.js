cc33edd93b360a1a6f0603874bcb86f80bcd4bb9
rene_imac <HarryMillerMail@googlemail.com>
2017-06-29 14:15:34 +0200

    array.find polyfill for IE11

--- src/util/polyfill.js
+++ src/util/polyfill.js
@@ -2,4 +2,31 @@
 // polyfill for Safari
 // https://stackoverflow.com/questions/22754315/for-loop-for-htmlcollection-elements
 HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
 
+// for IE11
+// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+/* eslint-disable */
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
